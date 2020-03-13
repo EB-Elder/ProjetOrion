353bf7b521f7485beae236efb2af698dcda51bc5
nelsonSulter <nelsonsulter91@gmail.com>
2020-03-13 18:37:40 +0100

    ECS Sphères qui bougent
    Ok ça bouge sans problème, y a un spawner et un system qui s'occupe de faire bouger toutes les sphère qui réponde à l'archetype.
    
    Bientôt les attaques de boss, ça va être lourd !
    
    Gang Gang

---
+++ Orion/Assets/Scripts/ECS/DataComponent/MovementData.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct MovementData : IComponentData
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float3 startPosition;
+
+    public bool goingUp;
+    public bool goingDown;
+
+    public KeyCode up;
+}
---
+++ Orion/Assets/Scripts/ECS/Systems/GrenadeSpawnerSystem.cs
@@ -0,0 +1,42 @@
+﻿
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class GrenadeSpawnerSystem : ComponentSystem
+{
+
+    private float spawnTimer;
+    private Random random;
+
+    protected override void OnCreate()
+    {
+        random = new Random(56);
+    }
+    protected override void OnUpdate()
+    {
+        spawnTimer -= Time.DeltaTime;
+        if (spawnTimer <= 0f)
+        {
+            spawnTimer = .5f;
+            float3 position = new float3(random.NextFloat(-5f, 5f), random.NextFloat(-5f, 5f), 0f);
+            //On accède à notre entity grace à la variable globale du PrefabEntityComponent
+            Entity spawnedEntity = EntityManager.Instantiate(PrefabEntityComponent.prefabEntity);
+
+            EntityManager.SetComponentData(spawnedEntity, new Translation { Value = position });
+            EntityManager.SetComponentData(spawnedEntity, new MovementData { startPosition = position, movementSpeed = 4, up = UnityEngine.KeyCode.Z, goingUp = true });
+
+
+            /*//On cherche dans toute les entity celles qui ont un component prefabEntityComponent
+            Entities.ForEach((ref PrefabEntityComponent prefabEntityComponent) =>
+            {
+                
+                Entity spawnedEntity = EntityManager.Instantiate(prefabEntityComponent.prefabEntity);
+
+                // Et ici on le place à une coordonnée random
+                EntityManager.SetComponentData(spawnedEntity, new Translation { Value = new float3(random.NextFloat(-5f, 5f), random.NextFloat(-5f, 5f), 0) });
+            });*/
+        }
+        
+    }
+}
