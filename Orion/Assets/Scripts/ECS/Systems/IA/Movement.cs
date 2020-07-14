56adb180322b4d9db72df4d9d0cf486dd9bf31ad
elhajjam.abdallah1@gmail.com <elhajjam.abdallah1@gmail.com>
2020-07-14 14:39:53 +0200

    LA VICTOIRE ON EST BIEN LA FAMAX

---
+++ Orion/Assets/Scripts/ECS/Systems/IA/Movement.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+public class Movement : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+
+        Entities.ForEach((ref Translation pos, in PlayerStatsData playerStatsData) =>
+        {
+            int toDebug = (int) pos.Value.x;
+
+            
+
+            if (pos.Value.x < 0)
+            {
+                float threshold = -0.5f;
+                if (toDebug + threshold < pos.Value.x)
+                {
+                    Debug.Log(toDebug + "With Treshold = " + threshold);
+                }
+                else if (toDebug + threshold > pos.Value.x)
+                {
+                    Debug.Log(toDebug + threshold + "With Treshold = " + threshold);
+                }
+            }
+            else
+            {
+                float threshold = 0.5f;
+                if (toDebug + threshold > pos.Value.x)
+                {
+                    Debug.Log(toDebug + "With Treshold = " + threshold);
+                }
+                else if (toDebug + threshold < pos.Value.x)
+                {
+                    Debug.Log(toDebug + threshold + "With Treshold = " + threshold);
+                }
+            }
+            
+            
+
+
+        }).Run();
+        return default;
+    }
+}
